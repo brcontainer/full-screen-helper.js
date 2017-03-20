7740aba75cd24a090f225e7c4596a8bf4210e1dc
Guilherme Nascimento <brcontainer@yahoo.com.br>
2017-03-19 22:23:47 -0300

    Version 0.2.0
    - Added FullScreenHelper.root function
    - Added FullScreenHelper.supported function
    - Restore scroll position in older IE version
    - Check if HTML element is valid
    - Now the :fullscreen, :full-screen, :-webkit-full-screen,
    :-moz-full-screen and :-ms-fullscree are dispensable (use
    .full-screen-helper).

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 ## Full Screen Helper
 
+### Browser support
+
+Tested in:
+
+- IE8+ (using `ActiveX`)
+- IE11 (without `ActiveX`)
+- Firefox
+- Google Chrome
+
+> **Note:** for security reasons maybe `WScript.shell` (`ActiveX`) may be blocked.
+
 ### Usage
+
+For use an video in fullscreen:
+
+```html
+<video id="myVideo" src="video.webm"></video>
+
+<button onclick="FullScreenHelper.request(document.getElementById('myVideo'));">Fullscreen</button>
+```
+
+Entire page in fullscreen:
+
+```html
+<button onclick="FullScreenHelper.request(document.body);">Fullscreen</button>
+```
+
+
+### API
 
 Method | Description
 --- | ---
@@ -8,9 +36,76 @@ Method | Description
 `FullScreenHelper.request(element)` | Show element in fullscreen, if there is not another one on fullscreen
 `FullScreenHelper.toggle(element)` | Put the element in fullscreen or restore
 `FullScreenHelper.exit()` | exit fullscreen mode
+`FullScreenHelper.root(element)` | If you have removed the scrollbar from the root element on the page and added it to an element within the page for some reason, you can set it here for better scrollbar control (only needed for older versions than IE11)
 
 ### Fullscreen in Internet Explorer
 
 For support in older MSIE browser is needed `WScript.Shell` activex, but in IE8+ security has been increased, which can cause the script to not work.
 
 For MSIE11 is used `msRequestFullscreen` and `msExitFullscreen`
+
+### CSS selector
+
+Use combined CSS selectors with prefix don't work, because when there is an invalid selector the whole rule dropped, if grouped (details: https://www.w3.org/TR/selectors4/#logical-combination):
+
+```css
+:fullscreen,
+:full-screen,
+:-webkit-full-screen,
+:-moz-full-screen,
+:-ms-fullscreen {
+    /* your custom css */
+}
+```
+
+When the selectors are not grouped, only invalids (depends on the browser) rules are dropped, example:
+
+```css
+/* dropped in older browsers */
+:fullscreen {
+    /* your custom css */
+}
+
+/* dropped in new browsers */
+:full-screen {
+    /* your custom css */
+}
+
+/* dropped in all browsers except Safari, Chrome and others with webkit */
+:-webkit-full-screen {
+    /* your custom css */
+}
+
+/* dropped in all browsers except Mozilla Firefox */
+:-moz-full-screen {
+    /* your custom css */
+}
+
+/* dropped in all browsers except Internet Explorer 11 */
+:-ms-fullscreen {
+    /* your custom css */
+}
+```
+
+However having a rule for each thing can make the code much more difficult to maintain, so you can use the selector:
+
+```css
+.full-screen-helper {
+    /* your custom css */
+}
+```
+
+### CSS box-sizing
+
+The script has not been tested on older browsers such as IE7, but is likely to work depending on `ActiveX` support, however `box-sizing` may not work or you may be using a modern browser and you have changed the control of `box-sizing` to something like:
+
+
+```css
+.my-element {
+    box-sizing: content-box !important;
+    padding: 10px !important;
+    border: 1px #ccc solid !important;
+}
+```
+
+In both cases you can suffer side effects maybe use `padding` or `border`.
--- full-screen-helper.js
+++ full-screen-helper.js
@@ -1,5 +1,6 @@
 /*
- * full-screen-helper.js
+ * full-screen-helper.js 0.2.0
+ *
  * Copyright (c) 2017 Guilherme Nascimento (brcontainer@yahoo.com.br)
  *
  * Released under the MIT license
@@ -8,14 +9,35 @@
 (function (d, w) {
     "use strict";
 
-    var wso,
+    var sy = 0,
+        sx = 0,
+        wso,
+        html,
+        body,
+        target,
         timer = 0,
-        clsRE = /(^|\s)full-screen-helper($|\s)/i,
-        currentElement;
+        currentElement,
+        detectOnMS = false,
+        clsRE = /(^|\s+)full-screen-helper($|\s+)/i,
+        clsNSRE = /(^|\s+)fsh-infullscreen($|\s+)/i,
+        eventsFS = [
+            "webkitfullscreenchange",
+            "mozfullscreenchange",
+            "fullscreenchange",
+            "MSFullscreenChange"
+        ];
+
+    function isHTMLElement(obj) {
+        if (w.HTMLElement) {
+            return obj instanceof w.HTMLElement;
+        }
+
+        return obj && obj.nodeType === 1 && obj.ownerDocument;
+    }
 
     function msDetectEscTrigger(e) {
         if (currentElement && wso) {
-            e = e || window.event;
+            e = e || w.event;
 
             if (e && e.keyCode == 27) {
                 wso.SendKeys("{F11}");
@@ -24,20 +46,52 @@
     }
 
     function msFS() {
-        var html, iw = w.innerWidth;
+        if (detectOnMS) {
+            var iw = w.outerWidth || w.innerWidth || d.width;
 
-        if (!iw) {
-            html = d.documentElement || (d.body && d.body.parentNode);
-            iw = html.clientWidth;
+            if (!html) {
+                return false;
+            }
+
+            if (!clsNSRE.test(html.className)) {
+                html.className += " fsh-infullscreen";
+            }
+
+            return (iw || html.clientWidth) == w.screen.width;
         }
 
-        return iw == w.screen.width;
+        return !!(
+            d.fullscreenElement ||
+            d.mozFullScreenElement ||
+            d.webkitFullscreenElement ||
+            d.msFullscreenElement ||
+            false
+        );
     }
 
-    function detectResize() {
+    function detectFSChange(e) {
         if (currentElement) {
             clearTimeout(timer);
-            timer = setTimeout(toggleFSClass, 100);
+            timer = setTimeout(toggleFSClass, detectOnMS ? 100 : 10);
+        }
+    }
+
+    function getMainElements() {
+        if (!html) {
+            body = d.body;
+            html = d.documentElement || (body && body.parentNode);
+        }
+    }
+
+    function getScrollPosition() {
+        getMainElements();
+
+        if (target) {
+            sx = target.scrollLeft;
+            sy = target.scrollTop;
+        } else {
+            sx = w.pageXOffset || w.scrollX || html.scrollLeft || body.scrollLeft || 0;
+            sy = w.pageYOffset || w.scrollY || html.scrollTop || body.scrollTop || 0;
         }
     }
 
@@ -53,6 +107,34 @@
         } else {
             currentElement.className = currentElement.className.replace(clsRE, " ");
             currentElement = null;
+
+            getMainElements();
+
+            if (html) {
+                html.className = html.className.replace(clsNSRE, " ");
+
+                if (target && (sy || sx)) {
+                    target.scrollLeft = sx;
+                    target.scrollTop = sy;
+                } else {
+                    w.scrollTo(sx, sy);
+                }
+            }
+        }
+    }
+
+    function getWSO() {
+        if (typeof wso === "undefined") {
+            try {
+                wso = new w.ActiveXObject("WScript.Shell");
+                d.attachEvent("onkeydown", msDetectEscTrigger);
+
+                detectOnMS = true;
+
+                w.attachEvent("onresize", detectFSChange);
+            } catch (ee) {
+                wso = null;
+            }
         }
     }
 
@@ -61,34 +143,43 @@
             wso = null;
         }
 
-        if (typeof wso === "undefined") {
-            try {
-                wso = new w.ActiveXObject("WScript.Shell");
-                d.attachEvent("onkeydown", msDetectEscTrigger);
-                w.attachEvent("onresize", detectResize);
-            } catch (ee) {
-                wso = null;
-            }
-        }
+        getWSO();
 
         if (wso !== null) {
             currentElement = element;
+
+            getScrollPosition();
+
             wso.SendKeys("{F11}");
         }
+    }
+
+    function isSupported() {
+        return !!(
+            detectOnMS ||
+            d.exitFullscreen ||
+            d.mozCancelFullScreen ||
+            d.webkitExitFullscreen ||
+            d.webkitCancelFullScreen ||
+            d.msExitFullscreen
+        );
     }
 
     function getElementInFS() {
         return d.fullscreenElement ||
                d.mozFullScreenElement ||
                d.webkitFullscreenElement ||
+               d.msFullscreenElement ||
                currentElement ||
                null;
     }
 
     function requestFS(element) {
-        if (!element || getElementInFS() !== null) {
+        if (!isHTMLElement(element) || getElementInFS() !== null) {
             return;
         }
+
+        currentElement = element;
 
         if (element.requestFullscreen) {
             element.requestFullscreen();
@@ -99,6 +190,7 @@
         } else if (element.msRequestFullscreen) {
             element.msRequestFullscreen();
         } else {
+            currentElement = null;
             return msToggleFS(element);
         }
     }
@@ -110,6 +202,8 @@
             d.mozCancelFullScreen();
         } else if (d.webkitExitFullscreen) {
             d.webkitExitFullscreen();
+        } else if (d.webkitCancelFullScreen) {
+            d.webkitCancelFullScreen();
         } else if (d.msExitFullscreen) {
             d.msExitFullscreen();
         } else if (wso && currentElement) {
@@ -125,10 +219,24 @@
         }
     }
 
-    window.FullScreenHelper = {
+    if (!d.attachEvent && d.addEventListener) {
+        for (var i = eventsFS.length - 1; i >= 0; i--) {
+            d.addEventListener(eventsFS[i], detectFSChange);
+        }
+
+        w.addEventListener("resize", detectFSChange);
+    }
+
+    w.FullScreenHelper = {
+        "supported": isSupported,
         "current": getElementInFS,
         "request": requestFS,
         "toggle": toggleFS,
-        "exit": exitFS
+        "exit": exitFS,
+        "root": function (element) {
+            if (isHTMLElement(element)) {
+                target = element;
+            }
+        }
     };
 })(document, window);
